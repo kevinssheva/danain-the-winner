5997cefed734e2ab722ea044b04f8f95c5fb6b61
kevinssheva <kevinssheva@gmail.com>
2023-09-28 12:56:17 +0700

    added login navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,7 @@ import { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import MenuIcon from "./MenuIcon";
+import { BsPersonCircle } from "react-icons/bs";
 
 const data = [
   {
@@ -22,6 +23,12 @@ const data = [
   },
 ];
 
+const userMenu = [
+  {
+    name: "Profile",
+    link: "/profile",
+  },
+];
 const Navbar = () => {
   const [isVisible, setIsVisible] = useState(true);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
@@ -29,6 +36,8 @@ const Navbar = () => {
   const pathName = usePathname();
   const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
+
+  const [user, setUser] = useState("Kevin");
 
   const isActive = useMemo(() => {
     return (link: string) => link === pathName;
@@ -54,7 +63,7 @@ const Navbar = () => {
     // Fungsi ini akan dipanggil setiap kali komponen di-mount
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
-      
+
       setIsVisible(currentScrollY < prevScrollPos);
       setPrevScrollPos(currentScrollY);
     };
@@ -72,8 +81,9 @@ const Navbar = () => {
     <div
       className={`fixed w-full py-4 top-0 z-50 px-[7%]
       ${isTransparent ? "bg-transparent " : "bg-background shadow-lg"}
-      ${isVisible || isOpen ? "translate-y-0" : " -translate-y-full"
-        } transition`}
+      ${
+        isVisible || isOpen ? "translate-y-0" : " -translate-y-full"
+      } transition`}
     >
       <div className="flex justify-between items-center">
         <Link href="/">
@@ -89,10 +99,11 @@ const Navbar = () => {
           {data.map((item) => (
             <li
               key={item.name}
-              className={`ml-6 ${isActive(item.link)
-                ? "border-b-[1px] border-white font-semibold"
-                : "font-normal"
-                } transition relative group`}
+              className={`ml-6 ${
+                isActive(item.link)
+                  ? "border-b-[1px] border-white font-semibold"
+                  : "font-normal"
+              } transition relative group`}
             >
               <Link
                 href={item.link}
@@ -111,21 +122,62 @@ const Navbar = () => {
           handleToggle={() => setIsOpen((prev) => !prev)}
         />
         <div className="md:flex hidden">
-          <Button text="Login" isPrimary onClick={() => { router.push("/login") }} />
+          {user ? (
+            <div className="border-2 relative border-white px-5 py-1 rounded-full group bg-background">
+              <div className="w-full flex gap-2">
+                <p>
+                  Hi, <span className="font-bold">{user}</span>
+                </p>
+                <BsPersonCircle size={24} />
+              </div>
+              <div
+                className={`absolute w-full left-0 top-1/2 rounded-md overflow-hidden -z-10 bg-background group-hover:scale-y-100 hover:scale-y-100 scale-y-0 transition-all origin-top`}
+              >
+                <div className="w-full pt-6">
+                  <ul className="flex flex-col ">
+                    {userMenu.map((item) => (
+                      <li key={item.name} className="cursor-pointer py-2 px-5">
+                        <Link
+                          href={item.link}
+                          className={`font-montserrat hover:underline`}
+                        >
+                          {item.name}
+                        </Link>
+                      </li>
+                    ))}
+                    <li className="cursor-pointer py-2 px-5 font-montserrat hover:underline">
+                      Logout
+                    </li>
+                  </ul>
+                </div>
+                <div className="w-full bg-primary h-1"></div>
+              </div>
+            </div>
+          ) : (
+            <Button
+              text="Login"
+              isPrimary
+              onClick={() => {
+                router.push("/login");
+              }}
+            />
+          )}
         </div>
       </div>
       <div
-        className={`w-full md:hidden h-screen absolute bg-background left-0 top-0 -z-10 origin-top ${isOpen ? "scale-y-100" : "scale-y-0"
-          } transition-all duration-300 px-[7%] pt-24 pb-10 flex flex-col justify-between`}
+        className={`w-full md:hidden h-screen absolute bg-background left-0 top-0 -z-10 origin-top ${
+          isOpen ? "scale-y-100" : "scale-y-0"
+        } transition-all duration-300 px-[7%] pt-24 pb-10 flex flex-col justify-between`}
       >
         <ul className="flex flex-col items-start gap-6">
           {data.map((item) => (
             <li
               key={item.name}
-              className={`${isActive(item.link)
-                ? "border-b-[2px] border-white font-semibold"
-                : "font-normal"
-                } transition relative group pb-1`}
+              className={`${
+                isActive(item.link)
+                  ? "border-b-[2px] border-white font-semibold"
+                  : "font-normal"
+              } transition relative group pb-1`}
             >
               <Link
                 href={item.link}
@@ -135,8 +187,43 @@ const Navbar = () => {
               </Link>
             </li>
           ))}
+          {userMenu.map((item) => (
+            <li
+              key={item.name}
+              className={`${
+                isActive(item.link)
+                  ? "border-b-[2px] border-white font-semibold"
+                  : "font-normal"
+              } transition relative group pb-1`}
+            >
+              <Link
+                href={item.link}
+                className={`font-montserrat text-xl text-white`}
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))}
+          <li className="text-xl font-montserrat">Logout</li>
         </ul>
-        <Button text="Login" fullWidth onClick={() => { router.push("/login") }} />
+        {user ? (
+          <div className="border-2 border-white px-5 py-2 rounded-full group bg-background">
+            <div className="w-full flex gap-3 text-xl">
+              <BsPersonCircle size={24} />
+              <p>
+                Hi, <span className="font-bold">{user}</span>
+              </p>
+            </div>
+          </div>
+        ) : (
+          <Button
+            text="Login"
+            fullWidth
+            onClick={() => {
+              router.push("/login");
+            }}
+          />
+        )}
       </div>
     </div>
   );
