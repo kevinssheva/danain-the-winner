b451f63990cd763e8abd9db5e892c06e0f2258b3
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2023-09-30 21:20:56 +0700

    fix: build error

--- src/app/actions/getAllTransaction.ts
+++ src/app/actions/getAllTransaction.ts
@@ -1,11 +1,14 @@
-import axios from "axios";
+// import axios from "axios";
 import { Transaction } from "@prisma/client";
+import { prisma } from "@/app/lib/prisma";
 
 export const getAllTransaction = async () => {
     try {
-        const { data } = await axios.get(process.env.NEXT_PUBLIC_WEB_URL + `/api/v1/transaction/}`);
+        // const { data } = await axios.get(`/api/v1/transaction/}`);
 
-        const { transactions }: { transactions: Transaction[] } = data;
+        // const { transactions }: { transactions: Transaction[] } = data;
+
+        const transactions = await prisma.transaction.findMany();
 
         return transactions;
     } catch (error) {
